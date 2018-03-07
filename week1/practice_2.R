35c70cd0efa80ca827ce28ff3de532320c48276b
Euni Cheng <d336643@gmail.com>
2018-03-08 07:23:53 +0800

    Undone Practice2,3
    See the wrong message on ReadMe.

--- week1/practice_2.R
+++ week1/practice_2.R
@@ -3,31 +3,31 @@
 
 
 # Create a vector called course.students.number, with data: c(1, 30)
-course.student.number <- 
+course.student.number <- c(1,30)
   
-  # Create a variable csn, with data: length of course.student.number
-  csn <- 
-  
-  # Create a vector course.student.grade, with sample() function: x = c(55:100), size = csn
-  course.student.grade <- 
-  
-  # Assign course.student.number as names of the course.student.grade
-  names(course.student.grade) <-
-  
-  # Create csg.mean, with the mean value of course.student.grade
-  csg.mean <- 
-  
-  # Create csg.max with the max value of course.student.grade
-  csg.max <- 
-  
-  # Create csg.min with the min value of course.student.grade
-  csg.min <- 
-  
-  # Create a vector csg.over.80, with the logical result of course.student.grade over 80
-  csg.over.80 <- 
-  
-  # Check csg.over.mean
-  csg.over.80
+# Create a variable csn, with data: length of course.student.number
+csn <- length((course.student.number))
+
+# Create a vector course.student.grade, with sample() function: x = c(55:100), size = csn
+course.student.grade <- sample(x = c(55:100), size = csn)
+
+# Assign course.student.number as names of the course.student.grade
+names(course.student.grade) <- course.student.number
+
+# Create csg.mean, with the mean value of course.student.grade
+csg.mean <- mean(course.student.grade)
+
+# Create csg.max with the max value of course.student.grade
+csg.max <- max(course.student.grade)
+
+# Create csg.min with the min value of course.student.grade
+csg.min <- min(course.student.grade)
+
+# Create a vector csg.over.80, with the logical result of course.student.grade over 80
+csg.over.80 <- course.student.grade >= 80
+
+# Check csg.over.mean
+csg.over.80
 
 # Filter the course.student.grade with csg.over.mean
 course.student.grade[csg.over.80]
@@ -39,5 +39,5 @@ print(paste("???????????????", csg.max))
 print(paste("???????????????", csg.min))
 
 # Print over 80 details
-# print(paste("??????80???????????????", ____________________))
-# print(paste("??????80????????????", ______________________))
+print(paste("??????80???????????????", length(csg.over.80)))
+print(paste("??????80????????????", names(course.student.grade[csg.over.80])))
